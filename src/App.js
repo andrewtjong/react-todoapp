33e8024348e060bc3c04ff2be3a8ed8128946793
andrewtjong <andrewttj@yahoo.com>
2020-10-17 15:49:29 +0200

    applied Class Based Components instead of Function Based Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,62 @@
 import React from "react";
 import TodoItem from "./components/TodoItem";
+import todosData from "./todosData";
 
-function App() {
-  return (
-    <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todoItems: todosData.map((item) => (
+        <TodoItem key={item.id} item={item} />
+      )),
+    };
+  }
+  render() {
+    return <div className="todo-list">{this.state.todoItems}</div>;
+  }
 }
 
 export default App;
+
+// import React from "react";
+// import TodoItem from "./components/TodoItem";
+// import todosData from "./todosData";
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       todosItems: todosData.map((item) => (
+//         <TodoItem key={item.id} item={item} />
+//       )),
+//     };
+//   }
+//   render() {
+//     return (
+//       <div className="todo-list">
+//         <p>{this.state.todosItems}</p>
+//       </div>
+//     );
+//   }
+// }
+
+// function App() {
+//   const todosItems = todosData.map((item) => (
+//     <TodoItem key={item.id} item={item} />
+//   ));
+//   return <div className="todo-list">{todosItems}</div>;
+// }
+
+// const todosList = todosData.map((todo) => (
+//   <TodoItem key={todo.id} text={todo.text} completed={todo.completed} />
+// ));
+
+// return <div className="todo-list">{todosList}</div>;
+// }
+// export default App;
+
+// const jokeComponents = jokesData.map((joke) => (
+//   <Joke key={joke.id} question={joke.question} punchJoke={joke.punchLine} />
+// ));
+
+// return <div>{jokeComponents}</div>;
